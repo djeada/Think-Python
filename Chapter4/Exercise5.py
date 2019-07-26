09b64a0cb8c0218c438b8630bcf71eae95604be4
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-07-26 22:32:33 +0100

    Add files via upload

---
+++ Chapter4/Exercise5.py
@@ -0,0 +1,24 @@
+'''
+Exercise 2
+Read about spirals at http://en.wikipedia.org/wiki/Spiral;
+then write a program that draws an Archimedian spiral
+(or one of the other kinds).
+'''
+import math
+import turtle
+
+def draw_spiral(turtle, n):
+    for i in range(n):
+        t = i / 20 * 3.14
+        x = (1 + 5 * t) * math.cos(t)
+        y = (1 + 5 * t) * math.sin(t)
+        turtle.goto(x, y)
+
+adam = turtle.Turtle()
+#adam.speed(100)
+
+draw_spiral(adam,300)
+
+turtle.exitonclick()
+
+
