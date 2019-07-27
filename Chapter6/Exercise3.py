2e6c5c71fb72727085b4070d7f4ce1381428e349
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-07-27 23:13:41 +0100

    Add files via upload

---
+++ Chapter6/Exercise3.py
@@ -0,0 +1,54 @@
+'''
+Exercise 3
+A palindrome is a word that is spelled the same backward
+and forward, like “noon” and “redivider”. Recursively,
+a word is a palindrome if the first and last letters are
+the same and the middle is a palindrome.
+
+The following are functions that take a string argument
+and return the first, last, and middle letters:
+
+def first(word):
+    return word[0]
+
+def last(word):
+    return word[-1]
+
+def middle(word):
+    return word[1:-1]
+We’ll see how they work in Chapter 8.
+
+Type these functions into a file named palindrome.py and
+test them out. What happens if you call middle with a string
+with two letters? One letter? What about the empty string,
+which is written '' and contains no letters?
+
+Write a function called is_palindrome that takes a string
+argument and returns True if it is a palindrome and False
+otherwise. Remember that you can use the built-in function
+len to check the length of a string.
+'''
+
+def first(word):
+    return word[0]
+
+def last(word):
+    return word[-1]
+
+def middle(word):
+    return word[1:-1]
+
+def is_palindrome(word):
+    if len(word) <= 1:
+        return True
+    if first(word) != last(word):
+        return False
+    return is_palindrome(middle(word))
+
+
+print(is_palindrome('kajak'))
+print(is_palindrome('damad'))
+print(is_palindrome('roor'))
+print(is_palindrome('villan'))
+
+
---
+++ Chapter6/Exercise4.py
@@ -0,0 +1,23 @@
+'''
+Exercise 4
+A number, a, is a power of b if it is divisible by b
+and a/b is a power of b. Write a function called is_power
+that takes parameters a and b and returns True if a is
+a power of b.
+Note: you will have to think about the base case.
+'''
+
+def is_power(a, b):
+    if a < b:
+         return False
+    if a == b:
+         return True
+    else:
+        return is_power(a / b, b)
+    
+print(is_power(8,2))
+print(is_power(125,5))
+print(is_power(99,2))
+
+
+
