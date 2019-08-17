cb5f0361e3a5dc38c2b6211d80d54889f56a6172
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-17 19:58:41 +0100

    Add files via upload

---
+++ Chapter7/Exercise1.py
@@ -0,0 +1,20 @@
+'''
+Exercise 1  
+Rewrite the function print_n from Section 5.8
+using iteration instead of recursion.
+'''
+
+#recursive solution
+def rec_print_n(s, n):
+    if n <= 0:
+        return
+    print(s)
+    rec_print_n(s, n-1)
+
+#itterative solution
+def it_print_n(s, n):
+    for i in range(n):
+        print(s)
+
+rec_print_n(10,4)
+it_print_n(10,4)
