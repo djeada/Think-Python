eba7ef149a265ff3833e3ebf7f8fcd53e43ee82c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-19 20:01:49 +0100

    Add files via upload

---
+++ Chapter9/Exercise3.py
@@ -0,0 +1,22 @@
+'''
+Exercise 3
+Write a function named avoids that takes a word and a string of
+forbidden letters, and that returns True if the word doesnt use any
+of the forbidden letters.
+'''
+
+def avoids(word, string):
+    for letter in string:
+        if letter in word:
+            return False
+    return True
+
+print(avoids('adam', 'madam'))
+print(avoids('zumbe', 'gandhi'))
+
+
+
+
+
+
+
