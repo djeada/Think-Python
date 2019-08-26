84f3a859f88ec6cd383bcdfca2c5cf8127f57887
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-08-27 00:58:21 +0200

    Add files via upload

---
+++ Chapter13/Exercise1.py
@@ -0,0 +1,30 @@
+'''
+Exercise 1  
+Write a program that reads a file, breaks each line into words, strips
+whitespace and punctuation from the words, and converts them to lowercase.
+
+Hint: The string module provides strings named whitespace, which contains
+space, tab, newline, etc., and punctuation which contains the punctuation
+characters. Let’s see if we can make Python swear:
+
+>>> import string
+>>> print string.punctuation
+!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+Also, you might consider using the string methods strip, replace and translate.
+'''
+from string import punctuation, whitespace
+
+with open('pride_and_prejudice.txt', 'r') as fd:
+    word_list = fd.read().split()
+
+def clean_word(word):
+    clean = ''
+    for c in word:
+        if not (c in punctuation or c in whitespace):
+            clean += c.lower()
+    return clean
+        
+word_list = [clean_word(x) for x in word_list]
+
+
+print(word_list[-10:])
---
+++ Chapter13/Exercise2.py
@@ -0,0 +1,45 @@
+'''
+Exercise 2  
+Go to Project Gutenberg (http://gutenberg.org) and
+download your favorite out-of-copyright book in plain
+text format.
+
+Modify your program from the previous exercise to read
+the book you downloaded, skip over the header information
+at the beginning of the file, and process the rest of the
+words as before.
+
+Then modify the program to count the total number of words
+in the book, and the number of times each word is used.
+
+Print the number of different words used in the book.
+Compare different books by different authors, written in
+different eras. Which author uses the most extensive vocabulary?
+'''
+from string import punctuation, whitespace
+
+with open('pride_and_prejudice.txt', 'r') as fd:
+    for line in fd:
+        if 'Chapter ' in line:
+            word_list = [line.rstrip('\n')]
+            for line in fd:
+                line = line.rstrip('\n')
+                word_list.append(line)
+
+def clean_word(word):
+    clean = ''
+    for c in word:
+        if not (c in punctuation or c in whitespace):
+            clean += c.lower()
+    return clean
+        
+word_list = [clean_word(x) for x in word_list]
+
+
+hist = {}
+for word in word_list:
+    hist[word] = hist.get(word, 0) + 1
+
+print('Total amount of words: %s' % len(word_list))
+print('Unique words: %s' % len(hist))
+                
