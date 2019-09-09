aca9462db7e8495b3d985d9fb9a538484b9da19a
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-09-09 09:27:27 +0200

    Add files via upload

---
+++ Chapter18/Exercise1.py
@@ -0,0 +1,35 @@
+'''
+Exercise 1
+Write a __cmp__ method for Time objects. Hint: you can use tuple comparison,
+but you also might consider using integer subtraction.
+
+'''
+
+class Time():
+    def __init__(self, hour=0, minute=0, second=0):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+    def __str__(self):
+        return str(self.hour) + ':' + str(self.minute) + ':' + str(self.second)
+
+    def time_to_int(self):
+        minutes = self.hour * 60 + self.minute
+        seconds = minutes * 60 + self.second
+        return seconds
+
+
+    def __gt__(self, other):
+        if self.hour > other.hour:
+            return True
+        elif self.hour == other.hour and self.minute > other.minute:
+            return True
+        elif self.hour == other.hour and self.minute == other.minute and self.second >= other.minute:
+            return True
+        return False
+
+time1 = Time(13,53,11)
+time2 = Time(13,55,11)
+
+print(time2>time1)
---
+++ Chapter18/Exercise2.py
@@ -0,0 +1,78 @@
+'''
+Exercise 2
+Write a Deck method named sort that uses the list method sort to sort the
+cards in a Deck. sort uses the __cmp__ method we defined to determine sort
+order.
+'''
+
+import random
+
+class Card():
+    def __init__(self, suit=0, rank=2):
+        self.suit = suit
+        self.rank = rank
+
+    suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+    rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7', 
+              '8', '9', '10', 'Jack', 'Queen', 'King']
+
+    def __str__(self):
+        return '%s of %s' % (Card.rank_names[self.rank], Card.suit_names[self.suit])
+
+    def __gt__(self, other):
+        if self.suit < other.suit:
+            return False
+        if self.suit == other.suit:
+            if self.rank < other.rank:
+                return False
+        return True
+
+    def __ls__(self, other):
+        if self.suit > other.suit:
+            return False
+        if self.suit == other.suit:
+            if self.rank > other.rank:
+                return False
+        return True
+        
+class Deck():
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return "\n".join(res)
+
+    def pop_card(self):
+        return self.cards.pop()
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def sort(self):
+        for j in range(len(self.cards)):
+            for i in range(len(self.cards)-1):
+                if self.cards[i] > self.cards[i+1]:
+                    temp = self.cards[i]
+                    self.cards[i] = self.cards[i+1]
+                    self.cards[i+1] = temp
+
+
+deck1 = Deck()
+deck1.shuffle()
+print('Before sorting: ')
+print(deck1)
+
+deck1.sort()
+print('\nAfter sorting: ')
+print(deck1)
+    
